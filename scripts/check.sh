3bb337ea31a53967d20148eea2a0b3b99d2cd7f0
iosmanthus <dengliming@pingcap.com>
2024-06-21 17:11:20 +0800

    merge upstream release-7.5 (#29)

--- scripts/check.sh
+++ scripts/check.sh
@@ -20,9 +20,8 @@ check-protos-compatible() {
     export PATH=$GOPATH/bin:$PATH
 
     if [ ! -f "$GOPATH/bin/protolock" ]; then
-        GO111MODULE=off go get github.com/nilslice/protolock/cmd/protolock
-	    GO111MODULE=off go install github.com/nilslice/protolock/cmd/protolock
-	fi
+        go install github.com/nilslice/protolock/cmd/protolock@latest
+    fi
 
     if protolock status -lockdir=scripts -protoroot=proto; then
         protolock commit -lockdir=scripts -protoroot=proto
